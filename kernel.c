16c76e50c5eb0d064193717a1463d85b51495b9f
Ivan Zlatanov <ivanff1@abv.bg>
2021-02-10 16:11:03 +0200

    Minor edits and .gitignore was added.

--- kernel.c
+++ kernel.c
@@ -1,3 +1,6 @@
+
+int current_line = 0;
+
 void clear_screen(char *video_memory_beggining_pointer)
 {
     int j = 0;
@@ -9,7 +12,7 @@ void clear_screen(char *video_memory_beggining_pointer)
     }
 }
 
-void write_string(char *video_memory_beggining_pointer, const char *string, int current_line)
+void write_string(char *video_memory_beggining_pointer, const char *string)
 {
     int memory_index = 80 * current_line * 2;
     int string_index = 0;
@@ -23,19 +26,19 @@ void write_string(char *video_memory_beggining_pointer, const char *string, int 
     }
 }
 
-void write_new_line_string(char *video_memory_beggining_pointer, const char *string, int current_line)
+void write_new_line_string(char *video_memory_beggining_pointer, const char *string)
 {
+    ++current_line;
+    write_string(video_memory_beggining_pointer, string);
 }
 
 void kernel_main(void)
 {
-    int current_line = 0;
-
     const char *opening_string = "The Kernel has been started successfully.";
     const char *sign_string_upper = "___________";
-    const char *sing_string_text = "| By Ivan |";
+    const char *sign_string_text = "| By Ivan |";
     const char *sign_string_lower = "|_________|";
-    const char *sing_string_stick = "     ||    ";
+    const char *sign_string_stick = "     ||    ";
 
     // begining of the video memory
     char *video_memory_beggining_pointer = (char *)0xb8000;
@@ -46,7 +49,12 @@ void kernel_main(void)
     // 25 lines, 80 columns each, 2 bytes per char
     clear_screen(video_memory_beggining_pointer);
 
-    write_string(video_memory_beggining_pointer, opening_string, current_line);
+    // prints init message
+    write_string(video_memory_beggining_pointer, opening_string);
+    write_new_line_string(video_memory_beggining_pointer, sign_string_upper);
+    write_new_line_string(video_memory_beggining_pointer, sign_string_text);
+    write_new_line_string(video_memory_beggining_pointer, sign_string_lower);
+    write_new_line_string(video_memory_beggining_pointer, sign_string_stick);
 
     return;
 }
