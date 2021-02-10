674cc84c19636c05cf15961a4d735e2fd722ed2b
Ivan Zlatanov <ivanff1@abv.bg>
2021-02-10 16:01:57 +0200

    Screen clearing and string printing were separated into functions.

--- kernel.c
+++ kernel.c
@@ -1,27 +1,52 @@
-void kernel_main(void)
+void clear_screen(char *video_memory_beggining_pointer)
 {
-    const char *opening_string = "The Kernel has been started successfully.";
-    char *video_memory_beggining_pointer = (char *)0xb8000;
-
-    unsigned int i = 0;
-    unsigned int j = 0;
-
+    int j = 0;
     while (j < 80 * 25 * 2)
     {
         video_memory_beggining_pointer[j] = ' ';
         video_memory_beggining_pointer[j + 1] = 0x07;
         j = j + 2;
     }
+}
 
-    j = 0;
+void write_string(char *video_memory_beggining_pointer, const char *string, int current_line)
+{
+    int memory_index = 80 * current_line * 2;
+    int string_index = 0;
 
-    while (opening_string[j] != '\0')
+    while (string[string_index] != '\0')
     {
-        video_memory_beggining_pointer[i] = opening_string[j];
-        video_memory_beggining_pointer[i + 1] = 0x02;
-        ++j;
-        i = i + 2;
+        video_memory_beggining_pointer[memory_index] = string[string_index];
+        video_memory_beggining_pointer[memory_index + 1] = 0x02;
+        ++string_index;
+        memory_index = memory_index + 2;
     }
+}
+
+void write_new_line_string(char *video_memory_beggining_pointer, const char *string, int current_line)
+{
+}
+
+void kernel_main(void)
+{
+    int current_line = 0;
+
+    const char *opening_string = "The Kernel has been started successfully.";
+    const char *sign_string_upper = "___________";
+    const char *sing_string_text = "| By Ivan |";
+    const char *sign_string_lower = "|_________|";
+    const char *sing_string_stick = "     ||    ";
+
+    // begining of the video memory
+    char *video_memory_beggining_pointer = (char *)0xb8000;
+
+    unsigned int i = 0;
+    unsigned int j = 0;
+
+    // 25 lines, 80 columns each, 2 bytes per char
+    clear_screen(video_memory_beggining_pointer);
+
+    write_string(video_memory_beggining_pointer, opening_string, current_line);
 
     return;
 }
