20624989cbe27776d638e9dfd673394df9b28e79
Ivan Zlatanov <ivanff1@abv.bg>
2021-02-13 13:45:10 +0200

    Video memory begginig pointer is now defined in kernel.c

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 build:
 	nasm -f elf32 bootloader.asm -o bootloader.o
-	gcc -m32  -ffreestanding -c kernel/kernel.c -o kc.o
-	ld -m elf_i386 -T linker.ld -o archiox kasm.o kc.o 
+	gcc -m32  -ffreestanding -c  kernel/kernel.c -o kc.o
+	ld -m elf_i386 -T linker.ld -o archiox bootloader.o kc.o 
 
 clean:
-	rm kasm.o
+	rm bootloader.o
 	rm kc.o
 	rm archiox
 	
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,3 +1,5 @@
+#define VIDEO_MEMORY_BEGGINING 0xb8000
+
 #define BLACK_BACKGROUND 0x07
 #define WHITE_TEXT 0x07
 #define GREEN_TEXT 0x02
@@ -9,19 +11,18 @@ int current_line = 0;
 
 void kernel_main(void)
 {
-
     // begining of the video memory
     char *video_memory_beggining_pointer = (char *)0xb8000;
 
     // 25 lines, 80 columns each, 2 bytes per char
-    clear_screen(video_memory_beggining_pointer, BLACK_BACKGROUND);
+    clear_screen(VIDEO_MEMORY_BEGGINING, BLACK_BACKGROUND);
 
     // prints init message
-    write_string(video_memory_beggining_pointer, SUCCESSFUL_KERNEL_LOADING_MESSAGE, GREEN_TEXT, current_line);
+    write_string(VIDEO_MEMORY_BEGGINING, SUCCESSFUL_KERNEL_LOADING_MESSAGE, GREEN_TEXT, current_line);
 
     for (int i = 0; i < 8; i++)
     {
-        write_new_line_string(video_memory_beggining_pointer, MADE_BY_SIGN[i], GREEN_TEXT, current_line);
+        write_new_line_string(VIDEO_MEMORY_BEGGINING, MADE_BY_SIGN[i], GREEN_TEXT, current_line);
     }
 
     return;
