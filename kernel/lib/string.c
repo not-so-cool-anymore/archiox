9ae43abadb39a7b4873fcc899d3899bb09944573
Ivan Zlatanov <ivanff1@abv.bg>
2021-07-01 15:25:04 +0300

    String compare implementation was added.

---
+++ kernel/lib/string.c
@@ -0,0 +1,27 @@
+/**
+ * cmp - Case insensitive compare two strings and return number of differences
+ * @first: The first string
+ * @second: The second string
+ * @len: The length to which the strings would be compared to
+ */
+int strcmp(const char *first, const char *second, int len)
+{
+    int diff_factor = 0;
+
+    if (!len)
+        return -1;
+
+    for (int i = 0; i <= len; i++)
+    {
+        if (tolower(first[i]) != tolower(second[i]))
+        {
+            diff_factor += 1;
+        }
+    }
+
+    return diff_factor;
+}
+
+char *strcpy(char *source, char *destination, int len)
+{
+}
---
+++ kernel/lib/string.h
@@ -0,0 +1,6 @@
+#ifndef STRING_H_
+#define STRING_H_
+
+int strcmp(const char *first, const char *second, int len);
+
+#endif
